0d177c117773e375fd948d8e59e77283506c8890
Aurimas <auruxxas.ar@gmail.com>
2022-01-10 16:20:23 +0000

    Testing formdata

---
+++ ImdbWebsite/ImdbWebsite/index.js
@@ -0,0 +1,15 @@
+﻿const movies = ko.observableArray([])
+ko.applyBindings()
+
+$(document).ready(function() {
+    console.log("ready")
+    $.ajax({
+        url: "http://192.168.160.58/imdb5000/api/movies",
+        context: "application/json",
+        method: "GET",
+        success: (response) => {
+            movies(response)
+        }
+    })
+});
+
