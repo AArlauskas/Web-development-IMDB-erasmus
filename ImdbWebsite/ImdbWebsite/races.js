147b5d2702729bdc0ad5a0f3d3b13219b15a3f4c
Aurimas <auruxxas.ar@gmail.com>
2022-01-17 11:33:18 +0000

    Migrating to Formula 1

---
+++ ImdbWebsite/ImdbWebsite/races.js
@@ -0,0 +1,26 @@
+﻿const races = ko.observableArray([])
+ko.applyBindings()
+
+$(document).ready(function() {
+    $.ajax({
+        url: "http://192.168.160.58/Formula1/api/races?page=1&pagesize=20",
+        context: "application/json",
+        method: "GET",
+        success: (response) => {
+            console.log(response)
+            races(response["List"])
+        }
+    })
+    $("#button-table-search").click(() => {
+        searchValue = document.getElementById("table-search").value;
+        $.ajax({
+            url: `http://192.168.160.58/Formula1/api/search/races?q=${searchValue}`,
+            method: "GET",
+            success: (response) => {
+                console.log(response)
+                races(response)
+            }
+        })
+    })
+});
+
