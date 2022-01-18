fd582a37e387803d610d95a0e1ddc84c09ca9444
Aurimas <auruxxas.ar@gmail.com>
2022-01-18 15:45:06 +0000

    race details and driver details

---
+++ ImdbWebsite/ImdbWebsite/driver-details.js
@@ -0,0 +1,42 @@
+﻿const races = ko.observableArray();
+const statistics = ko.observableArray();
+ko.applyBindings()
+
+$(document).ready(() => {
+    const parameters = new URLSearchParams(window.location.search);
+    const id = parameters.get("id");
+
+    $.ajax({
+        url: `http://192.168.160.58/Formula1/api/drivers/driver?id=${id}`,
+        context: "application/json",
+        method: "GET",
+        success: (response) => {
+            console.log(response)
+            if (response["ImageUrl"] !== null) {
+                document.getElementById("driver-img").setAttribute("src", response["ImageUrl"])
+            }
+            document.getElementById("wiki-url").setAttribute("href", response["Url"]);
+            document.getElementById("driver-name").innerHTML = response["Name"];
+            document.getElementById("driver-nationality").innerHTML = `Nationality: ${response['Nationality']}`;
+            races(response["Races"])
+        }
+    });
+
+    $.ajax({
+        url: `http://192.168.160.58/Formula1/api/statistics/driver?id=${id}`,
+        context: "application/json",
+        method: "GET",
+        success: (response) => {
+            console.log(response)
+            document.getElementById("driver-races-count").innerHTML = `Races: ${response['Races']}`;
+            document.getElementById("driver-races-win").innerHTML = `Wins: ${response['Wins']}`;
+            statistics(response["Career"]);
+            //races(response["Races"])
+        }
+    });
+})
+
+const onTableRowClick = (data) => {
+    const raceId = data["RaceId"];
+    window.location.href = `race-details.html?id=${raceId}`
+}
---
+++ ImdbWebsite/ImdbWebsite/race-details.js
@@ -0,0 +1,26 @@
+﻿const results = ko.observableArray();
+ko.applyBindings()
+$(document).ready(() => {
+    const parameters = new URLSearchParams(window.location.search);
+    const id = parameters.get("id");
+
+    $.ajax({
+        url: `http://192.168.160.58/Formula1/api/races/race?id=${id}`,
+        context: "application/json",
+        method: "GET",
+        success: (response) => {
+            console.log(response)
+            document.getElementById("wiki-url").setAttribute("href", response["Url"]);
+            document.getElementById("race-name").innerHTML = response["Name"];
+            document.getElementById("race-circuit-name").innerHTML = `Circuit: ${response["Name"]}`;
+            document.getElementById("race-date").innerHTML = response["Date"].split("T")[0];
+
+            results(response["Results"].filter(el => el.Position < 10))
+        }
+    });
+})
+
+const onTableRowClick = (data) => {
+    const driverId = data["DriverId"];
+    window.location.href = `driver-details.html?id=${driverId}`
+}
