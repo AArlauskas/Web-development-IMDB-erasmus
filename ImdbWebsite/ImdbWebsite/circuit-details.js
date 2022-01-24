bf45c5191d556ab11359047de513b6b4aecc27e1
Aurimas <auruxxas.ar@gmail.com>
2022-01-24 12:05:19 +0000

    circuit details

---
+++ ImdbWebsite/ImdbWebsite/circuit-details.js
@@ -0,0 +1,31 @@
+﻿const races = ko.observableArray();
+ko.applyBindings();
+
+$(document).ready(() => {
+    const parameters = new URLSearchParams(window.location.search);
+    const id = parameters.get("id");
+
+    $.ajax({
+        url: `http://192.168.160.58/Formula1/api/circuits/circuit?id=${id}`,
+        context: "application/json",
+        method: "GET",
+        success: (response) => {
+            console.log(response)
+            if (response["ImageUrl"] !== null) {
+                document.getElementById("circuit-img").setAttribute("src", response["ImageUrl"])
+            }
+            document.getElementById("wiki-url").setAttribute("href", response["Url"]);
+            document.getElementById("circuit-name").innerHTML = response["Name"];
+            document.getElementById("circuit-country").innerHTML = `Country: ${response["Country"]}`;
+            document.getElementById("circuit-location").innerHTML = `Location: ${response["Location"]}`;
+
+            races(response["Races"]);
+
+        }
+    });
+});
+
+const onTableRowClick = (data) => {
+    const raceId = data["RaceId"];
+    window.location.href = `race-details.html?id=${raceId}`
+}
