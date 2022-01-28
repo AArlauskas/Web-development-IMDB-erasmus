3e32524870e01b25e9e730e4c3368ef262294c3a
Aurimas <auruxxas.ar@gmail.com>
2022-01-28 15:31:42 +0000

    Friday changes

---
+++ ImdbWebsite/ImdbWebsite/cchampions.js
@@ -0,0 +1,19 @@
+﻿const table = ko.observableArray([])
+ko.applyBindings()
+
+$(document).ready(function () {
+    $.ajax({
+        url: "http://192.168.160.58/Formula1/api/statistics/cchampions",
+        context: "application/json",
+        method: "GET",
+        success: (response) => {
+            console.log(response)
+            table(response)
+        }
+    })
+});
+
+const onTableRowClick = (data) => {
+    const constructorId = data["ConstructorId"];
+    window.location.href = `constructor-details.html?id=${constructorId}`
+}
---
+++ ImdbWebsite/ImdbWebsite/champions.js
@@ -0,0 +1,19 @@
+﻿const table = ko.observableArray([])
+ko.applyBindings()
+
+$(document).ready(function () {
+    $.ajax({
+        url: "http://192.168.160.58/Formula1/api/statistics/champions",
+        context: "application/json",
+        method: "GET",
+        success: (response) => {
+            console.log(response)
+            table(response)
+        }
+    })
+});
+
+const onTableRowClick = (data) => {
+    const driverId = data["DriverId"];
+    window.location.href = `driver-details.html?id=${driverId}`
+}
